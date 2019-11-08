f0a28ddc8f3130b17756a850b3c169a89c8163dd
Vicradon <vicraph7@gmail.com>
2019-11-08 14:34:20 +0100

    deploying demo branch

--- sw.js
+++ sw.js
@@ -1,56 +1,56 @@
 // STEP 5: UNCOMMENT THE staticCache
-const staticCache = 'static-v1';
+// const staticCache = 'static-v1';
 
 // STEP 8: UNCOMMENT THE dynamicCache
-const dynamicCache = 'dynamic-v1';
+// const dynamicCache = 'dynamic-v1';
 
 // STEP 6: UNCOMMENT THE APP SHELL ARRAY
-const appShell = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/script.min.js',
-  '/pages/offline.html'
-]
+// const appShell = [
+//   '/',
+//   '/index.html',
+//   '/css/style.css',
+//   '/js/script.min.js',
+//   '/pages/offline.html'
+// ]
 
 // STEP 4: UNCOMMENT THE INSTALL HANDLER LINES 18 TO 23
 // This is where we precache.
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(staticCache)
-      .then(cache => cache.addAll(appShell))
-  )
-})
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(staticCache)
+//       .then(cache => cache.addAll(appShell))
+//   )
+// })
 
 // STEP 7: UNCOMMENT THE ACTIVATE HANDLER
 // This is where we delete old service workers
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys(keyArr => {
-      keyArr
-        .filter(keys => keys !== staticCache && keys !== dynamicAssets)
-        .map(oldKey => caches.delete(oldKey))
-    })
-  )
-})
+// self.addEventListener('activate', event => {
+//   event.waitUntil(
+//     caches.keys(keyArr => {
+//       keyArr
+//         .filter(keys => keys !== staticCache && keys !== dynamicAssets)
+//         .map(oldKey => caches.delete(oldKey))
+//     })
+//   )
+// })
 
 
 // STEP 9: UNCOMMENT THE FETCH EVENT HANDLER
 // This is where we dynamically cache assets and also
 // serve files to the client
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .then(fetchResponse => caches.open(dynamicCache)
-        .then(cache => {
-          cache.put(event.request.url, fetchResponse.clone())
-          return fetchResponse
-        }
-      )
-    ).catch(() => caches.match('/pages/offline.html'))
-  )
-})
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => response || fetch(event.request))
+//       .then(fetchResponse => caches.open(dynamicCache)
+//         .then(cache => {
+//           cache.put(event.request.url, fetchResponse.clone())
+//           return fetchResponse
+//         }
+//       )
+//     ).catch(() => caches.match('/pages/offline.html'))
+//   )
+// })
 
 
 
