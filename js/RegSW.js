26286b3db2d52005511b6e1eef72db9ca45cd335
Vicradon <vicraph7@gmail.com>
2019-11-08 09:45:24 +0100

    about to deploy to github

---
+++ js/RegSW.js
@@ -0,0 +1,5 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(regObj => console.log("Service worker registered"))
+    .catch(err => console.log("An error occured", err))
+}
