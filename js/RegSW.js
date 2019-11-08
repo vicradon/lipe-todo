21fb585e131a358e375eac9dc15e7afde8e7422e
Vicradon <vicraph7@gmail.com>
2019-11-08 14:08:40 +0100

    redeploying, again

--- js/RegSW.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(regObj => console.log("Service worker registered"))
-    .catch(err => console.log("An error occured", err))
-}
